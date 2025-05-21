f16bf449d0e56924b1dee9d2edec1f8e3cd6a229
teauteau <contact@toteau.nl>
2025-05-21 22:16:13 +0200

    staticfiles

--- MorphoAi/MorphoAi/settings.py
+++ MorphoAi/MorphoAi/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 
 # For production (DEBUG=False)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'webapp/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
